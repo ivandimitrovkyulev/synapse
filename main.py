1569b6dab8f99b19ae59694e7edb04e5cc8803d1
IK <ivandkyulev@gmail.com>
2022-07-16 23:09:03 +0100

    Code improvement and bug fix

--- main.py
+++ main.py
@@ -9,13 +9,10 @@ from datetime import datetime
 
 from src.synapse.exceptions import exit_handler
 from src.synapse.interface import args
-from src.synapse.common import parse_args
-from src.synapse.api import get_bridge_output
-from src.synapse.message import telegram_send_message
-from src.synapse.logger import log_arbitrage
-from src.synapse.variables import (
-    time_format,
-    network_ids,
+from src.synapse.variables import time_format
+from src.synapse.common import (
+    parse_args,
+    arbitrage_alert,
 )
 
 
@@ -36,34 +33,10 @@ pprint(info)
 if args.screen:
 
     while True:
+
         start = perf_counter()
         for arg in parse_args(info):
-            min_arbitrage, coin, *func_args = arg
-
-            # Query swap amount out
-            swap_amount = get_bridge_output(*func_args)
-            # Execute only if swap_amount is Not None, eg. get request was successful
-            if swap_amount and swap_amount >= min_arbitrage:
-
-                decimals_in, chain_id_in, token_in = arg[3]
-                decimals_out, chain_id_out, token_out = arg[4]
-                network_in = network_ids[str(chain_id_in)]
-                network_out = network_ids[str(chain_id_out)]
-                timestamp = datetime.now().astimezone().strftime(time_format)
-
-                amount = func_args[0]
-                arbitrage = round((swap_amount - amount), int(decimals_in / 3))
-
-                message = f"{timestamp}\n" \
-                          f"Sell {amount:,} {token_in} {network_in} -> {network_out}\n" \
-                          f"\t--->Arbitrage: <a href='https://synapseprotocol.com'>{arbitrage:,} {token_out}</a>"
-
-                ter_msg = f"Sell {amount:,} {token_in} {network_in} -> {network_out}\n" \
-                          f"--->Arbitrage: {arbitrage:,} {token_out}"
-
-                telegram_send_message(message)
-                log_arbitrage.info(ter_msg)
-                print(ter_msg)
+            arbitrage_alert(arg)
 
         sleep(10)
         end = perf_counter()
--- src/synapse/common.py
+++ src/synapse/common.py
@@ -1,5 +1,14 @@
-from typing import List
+from datetime import datetime
 from itertools import permutations
+from typing import List
+
+from src.synapse.api import get_bridge_output
+from src.synapse.message import telegram_send_message
+from src.synapse.logger import log_arbitrage
+from src.synapse.variables import (
+    time_format,
+    network_ids,
+)
 
 
 def parse_args(schema: dict) -> List[list]:
@@ -25,3 +34,31 @@ def parse_args(schema: dict) -> List[list]:
             args.append(temp_list)
 
     return args
+
+
+def arbitrage_alert(arguments: List) -> None:
+    min_arbitrage, coin, *func_args = arguments
+    amount = func_args[0]
+
+    # Query swap amount out
+    swap_amount = get_bridge_output(*func_args)
+    # Execute only if swap_amount is Not None, eg. get request was successful
+    if swap_amount and ((swap_amount - amount) >= min_arbitrage):
+        decimals_in, chain_id_in, token_in = arguments[3]
+        decimals_out, chain_id_out, token_out = arguments[4]
+        network_in = network_ids[str(chain_id_in)]
+        network_out = network_ids[str(chain_id_out)]
+        timestamp = datetime.now().astimezone().strftime(time_format)
+
+        arbitrage = round((swap_amount - amount), int(decimals_in / 3))
+
+        message = f"{timestamp}\n" \
+                  f"Sell {amount:,} {token_in} {network_in} -> {network_out}\n" \
+                  f"\t--->Arbitrage: <a href='https://synapseprotocol.com'>{arbitrage:,} {token_out}</a>"
+
+        ter_msg = f"Sell {amount:,} {token_in} {network_in} -> {network_out}\n" \
+                  f"--->Arbitrage: {arbitrage:,} {token_out}"
+
+        telegram_send_message(message)
+        log_arbitrage.info(ter_msg)
+        print(ter_msg)
